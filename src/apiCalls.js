d309ffb0f2ffae2453ffa1ee427ff3474d6aa9d8
Noah <noahhughesph@gmail.com>
2023-05-05 10:33:56 -0400

    ensures proper data collection from APIs on click of SerachBtn component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,23 @@
 import { regionCodeToCountry } from './functions';
 
 const logAgifyData = async (name, country) => {
+  const countryStr = await logCountriesData(country);
   const agifyResponse = await fetch(
-    `https://api.agify.io?name=${name}&country_id=${country}`
+    `https://api.agify.io?name=${name}&country_id=${countryStr}`
   );
   const agifyJson = await agifyResponse.json();
-  return agifyJson.age === null ? 'Not Enough Data' : agifyJson.age;
+  return (await agifyJson.age) === null
+    ? 'Not Enough Data'
+    : agifyJson.age;
 };
 
 const logGenderizaData = async (name, country) => {
+  const countryStr = await logCountriesData(country);
   const genderizeResponse = await fetch(
-    `https://api.genderize.io?name=${name}&country_id=${country}`
+    `https://api.genderize.io?name=${name}&country_id=${countryStr}`
   );
   const genderizeJson = await genderizeResponse.json();
-  return genderizeJson.gender === null
+  return (await genderizeJson.gender) === null
     ? 'Not Enough Data'
     : genderizeJson.gender + ' ' + genderizeJson.probability + '%';
 };
@@ -23,7 +27,9 @@ const logNationalizeData = async (name) => {
     `https://api.nationalize.io?name=${name}`
   );
   const nationalizeJson = await nationalizeResponse.json();
-  return regionCodeToCountry(nationalizeJson.country[0].country_id);
+  return regionCodeToCountry(
+    await nationalizeJson.country[0].country_id
+  );
 };
 
 const logCountriesData = async (country) => {
@@ -31,7 +37,7 @@ const logCountriesData = async (country) => {
     `https://restcountries.com/v3.1/name/${country}`
   );
   const countriesJson = await countriesResponse.json();
-  return countriesJson[0].cca2;
+  return await countriesJson[0].cca2;
 };
 
 export const apiCalls = {
--- src/functions.js
+++ src/functions.js
@@ -12,15 +12,19 @@ export const nameFormatting = (nameStr) => {
   return nameStr.charAt(0).toUpperCase() + nameStr.slice(1);
 };
 
-export const outputFunction = (state) => {
+export const outputFunction = async (state) => {
   const returnArray = [];
   const countryStr = state.country;
   returnArray.push(nameFormatting(state.name));
   returnArray.push(countryStr);
 
-  returnArray.push(apiCalls.logAgifyData(state.name, countryStr));
-  returnArray.push(apiCalls.logNationalizeData(state.name));
-  returnArray.push(apiCalls.logGenderizaData(state.name, countryStr));
+  returnArray.push(
+    await apiCalls.logAgifyData(state.name, countryStr)
+  );
+  returnArray.push(await apiCalls.logNationalizeData(state.name));
+  returnArray.push(
+    await apiCalls.logGenderizaData(state.name, countryStr)
+  );
   console.log(returnArray);
   return returnArray;
 };
